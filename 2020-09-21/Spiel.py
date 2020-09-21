f5f52a957ad7326f821462995df0f93aef586418
David Helwich <helwichd0@gmail.com>
2020-09-21 16:48:36 +0200

    3.2.2.2.7

---
+++ 2020-09-21/Spiel.py
@@ -0,0 +1,62 @@
+#----------------------------------------
+# Wuerfel
+#----------------------------------------
+
+from random import randint
+
+class Wuerfel(object):
+    def __init__(self):
+        self.augen = randint(1, 6)
+
+    def werfen(self):
+        self.augen = randint(1, 6)
+
+    def getAugen(self):
+        return self.augen
+
+#----------------------------------------
+# Spieler
+#----------------------------------------
+
+class Spieler(object):
+    def __init__(self, pName):
+        self.name = pName
+        self.wuerfel = None
+        self.punkte = 0
+
+    def setWuerfel(self, pWuerfel):
+        self.wuerfel = pWuerfel
+        
+    def wuerfeln(self):
+        self.wuerfel.werfen()
+        self.punkte = self.punkte + self.wuerfel.getAugen()
+
+    def getName(self):
+        return self.name
+
+    def getPunkte(self):
+        return self.punkte
+
+#----------------------------------------
+# Test
+#----------------------------------------
+
+wuerfel = Wuerfel()
+
+spieler1 = Spieler('Zauberhand')
+spieler1.setWuerfel(wuerfel)
+
+spieler2 = Spieler('Looser')
+spieler2.setWuerfel(wuerfel)
+
+spieler3 = Spieler('Spider')
+spieler3.setWuerfel(wuerfel)
+
+for i in range(5):
+    spieler1.wuerfeln()
+    spieler2.wuerfeln()
+    spieler3.wuerfeln()
+    print('Punktestand', spieler1.getName(), ':', spieler1.getPunkte())
+    print('Punktestand', spieler2.getName(), ':', spieler2.getPunkte())
+    print('Punktestand', spieler3.getName(), ':', spieler3.getPunkte())
+    print()
