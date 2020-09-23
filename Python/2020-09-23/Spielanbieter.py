0060caf86a6dd0e7aa25de4cbdb84c955ee35733
David Helwich <helwichd0@gmail.com>
2020-09-23 15:22:27 +0200

    -

--- Python/2020-09-23/Spielanbieter.py
+++ Python/2020-09-23/Spielanbieter.py
@@ -10,16 +10,21 @@ class Spielanbieter(object):
         self.spielfeld = Spielfeld()
         self.spielerListe = []
         self.spielerAnReihe = 0
+        self.wuerfelListe = [self.wuerfel1, self.wuerfel2, self.wuerfel3]
         
     def spielerErzeugen(self):
         self.spielerListe.append(Spieler(self.wuerfel1, self.wuerfel2, self.wuerfel3, self.spielfeld))
 
     def naechsterSpieler(self):
-        self.spielerListe[self.spielerAnReihen].spielen()
+        self.spielerListe[self.spielerAnReihe].spielen()
         if len(self.spielerListe) >= spielerAnReihe + 1:
             self.spielerAnReihen = 0
         else:
             self.spielerAnReihe += 1
             
     def gewinnAuszahlung(self):
-        pass
+        richtige = 0
+        for i in self.wuerfelListe:
+            if i.getAugen() == self.spielfeld.getGesetzteZahl():
+                richtige += 1
+        self.spielerListe[self.spielAnReihe].einzahlen(1 * richtige)
