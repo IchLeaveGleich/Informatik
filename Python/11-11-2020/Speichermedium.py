075aea0c4877665776180e3efafa5f0d25c43501
IchLeaveGleich <50103677+IchLeaveGleich@users.noreply.github.com>
2020-11-11 09:25:04 +0100

    Add files via upload

---
+++ Python/11-11-2020/Speichermedium.py
@@ -0,0 +1,44 @@
+class Speichermedium(object):
+    def __init__(self):
+        self.__kapazitaet = 0.0
+
+    def setKapazitaet(self, kap):
+        self.kapazitaet = kap
+
+    def getKapazitaet(self):
+        return self.kapazitaet
+
+class CD(Speichermedium):
+    def __init__(self):
+        super().__init__()
+        self.__titel = ""
+        self.__anazahlLieder = 0
+        self.__liednummer = 0
+
+    def getTitle(self):
+        return self.title
+
+    def naechstesLied(self):
+        pass
+
+    def voherigesLied(self):
+        pass
+
+    def getLiednummer(self):
+        return self.liednummer
+
+    def setLiednummer(self, nr):
+        self.liednummer = nr
+
+
+class DVD(Speichermedium):
+    def __init__(self):
+        super().__init__()
+        self.aufloesung = ""
+        self.laserfarbe = ""
+
+    def setLaserfarbe(self, neueFarbe):
+        self.laserfarbe = neueFarbe
+
+    def getLaserfarbe(self):
+        return self.laserfarbe
